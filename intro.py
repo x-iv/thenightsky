4bb8f887f7be37b74aaa40f56c2d4fae24eeae48
x-iv <franktapira49@gmail.com>
2024-05-15 18:51:22 +0100

    18:51 15/5/24

--- intro.py
+++ intro.py
@@ -1,3 +1,8 @@
 #import the library and download the data
+#to save images and plot use matplotlib
+import matplotlib.pyplot as plt
 from lightkurve import search_targetpixelfile
 pixelfile = search_targetpixelfile("KIC 8462852", quarter=16).download(quality_bitmask='hardest');
+
+pixelfile.plot(frame=1);
+plt.savefig('frame1')
