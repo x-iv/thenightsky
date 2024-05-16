b82cb11fdaa6993f598ef8ed05b22e52160bfbcc
x-iv <franktapira49@gmail.com>
2024-05-16 20:43:43 +0100

    2043 16/5/24

--- planet_6922244.py
+++ planet_6922244.py
@@ -6,4 +6,21 @@ pixelfile = search_targetpixelfile("KIC 6922244", quarter=4).download();
 pixelfile.plot(frame=1000)
 plt.savefig('framep1000')
 
+lc = pixelfile.to_lightcurve(aperture_mask=pixelfile.pipeline_mask)
+lc.plot();
+plt.savefig('nonlinedplot')
 
+
+#flat_lightcurve
+flat_lc = lc.flatten(window_length=401)
+flat_lc.plot();
+plt.savefig('flattenlightcurve')
+
+#fold light curve
+#when we fold we put the lines together i.e each period which is 3.522
+#to identify period we use the periodogram
+folded_lc = flat_lc.fold(period=3.522)
+folded_lc.plot()
+plt.savefig('folded')
+
+#periodogram example
