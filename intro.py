76853afbe3b8e8481f995be8758228f590f21d68
x-iv <franktapira49@gmail.com>
2024-05-16 14:50:20 +0100

    14:50 16/5/24

--- intro.py
+++ intro.py
@@ -1,8 +1,21 @@
 #import the library and download the data
 #to save images and plot use matplotlib
+import numpy as np
 import matplotlib.pyplot as plt
 from lightkurve import search_targetpixelfile
 pixelfile = search_targetpixelfile("KIC 8462852", quarter=16).download(quality_bitmask='hardest');
 
 pixelfile.plot(frame=1000);
 plt.savefig('frame1000')
+
+lc = pixelfile.to_lightcurve(aperture_mask='all');
+#objects
+time_values = lc.time.value
+flux_values = lc.flux.value
+
+# Write lc.time and lc.flux to a text file
+np.savetxt('lc_data.txt', np.column_stack((time_values, flux_values)), header='Time Flux', fmt='%f %f', comments='')
+# 'lc_data.txt' is the name of the file where data will be saved
+# header='Time Flux' specifies the column headers in the text file
+# fmt='%f %f' specifies the format of data (floating-point numbers)
+# comments='' specifies that no comment string will be prepended to each line
