1d15669d899241e949905c1008499ee351fc09b2
x-iv <franktapira49@gmail.com>
2024-05-16 18:45:32 +0100

    18:45 16/5/24

---
+++ planet_6922244.py
@@ -0,0 +1,9 @@
+from lightkurve import search_targetpixelfile
+import numpy as np
+import matplotlib.pyplot as plt
+pixelfile = search_targetpixelfile("KIC 6922244", quarter=4).download();
+
+pixelfile.plot(frame=1)
+plt.savefig('framep1')
+
+
