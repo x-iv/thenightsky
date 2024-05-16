5640cacbfbc487c95a430ceeb4d2ac3a8dc57009
x-iv <franktapira49@gmail.com>
2024-05-16 14:31:30 +0100

    14:31 16/5/24

--- intro.py
+++ intro.py
@@ -4,5 +4,5 @@ import matplotlib.pyplot as plt
 from lightkurve import search_targetpixelfile
 pixelfile = search_targetpixelfile("KIC 8462852", quarter=16).download(quality_bitmask='hardest');
 
-pixelfile.plot(frame=1);
-plt.savefig('frame1')
+pixelfile.plot(frame=1000);
+plt.savefig('frame1000')
